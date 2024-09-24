195c9c9f2a3aa71a2057100f78ff3888554481dd
Jessy <jessy.pinchon@wildcodeschool.com>
2024-09-24 15:05:15 +0200

    corrigé workshop js dom

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("hello world!");
+// console.log(document.getElementsByTagName("button"));
+// console.log(document.getElementsByClassName("television__button"));
+// console.log(document.querySelector(".television__button"));
+// console.log(document.querySelectorAll(".television__button"));
+
+const buttons = document.querySelectorAll(".television__button");
+
+for (const button of buttons) {
+  button.addEventListener("click", () => {
+    alert("Clic !");
+  });
+}
+
+const buttonOn = document.querySelector(".television__button--on");
+
+buttonOn.addEventListener("click", () => {
+  const img = document.querySelector(".television__content");
+  img.src = "https://picsum.photos/id/237/270/190?grayscale";
+});
+
+const buttonOff = document.querySelector(".television__button--off");
+
+buttonOff.addEventListener("click", () => {
+  const img = document.querySelector(".television__content");
+  img.src =
+    "data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==";
+});
+
+const channelGrayscale = document.createElement("li");
+channelGrayscale.classList.add("television__channel");
+
+const channels = document.querySelector(".television__channels");
+channels.appendChild(channelGrayscale);
+
+const buttonGrayscale = document.createElement("button");
+buttonGrayscale.type = "button";
+buttonGrayscale.textContent = "T";
+
+channelGrayscale.appendChild(buttonGrayscale);
+
+const img = document.querySelector(".television__content");
+
+buttonGrayscale.addEventListener("click", () => {
+  const isData = img.src.startsWith("data");
+
+  if (!isData) {
+    const isGrayscale = img.src.endsWith("?grayscale");
+
+    if (isGrayscale) {
+      img.src = img.src.slice(0, -"?grayscale".length);
+    } else {
+      img.src += "?grayscale";
+    }
+  }
+});
